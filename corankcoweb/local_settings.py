e57fbaf4e00351858aef2a8f19631c9319ac3510
andrieu <pierre.andrieu@lri.fr>
2023-07-24 10:39:07 +0200

    separate local_settings from settings

---
+++ corankcoweb/local_settings.py
@@ -0,0 +1,70 @@
+from pathlib import Path
+from decouple import Config
+from dotenv import load_dotenv
+
+# path of env variables
+env_path = "/home/pierre/workspace/corankcoweb/.env"
+load_dotenv(dotenv_path="/home/pierre/workspace/corankcoweb/.env")
+# config Object with right path
+config = Config(env_path)
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = config('SECRET_KEY')
+
+# Allowed Hosts
+ALLOWED_HOSTS = config('ALLOWED_HOSTS_CORANKCOWEB', default='localhost').split(',')
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = config("DEBUG", default=False, cast=bool)
+
+# Email configurations
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST
+ADMINS = [(config('ADMIN_NAME'), config('ADMIN_EMAIL'))]
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'auth_app',
+    'corankcoweb'
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('NAME_CORANKCOWEB_DB'),
+        'USER': config('USER_ADMIN_CORANKCOWEB_DB'),
+        'PASSWORD': config('PASSWORD_ADMIN_CORANKCOWEB_DB'),
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+
